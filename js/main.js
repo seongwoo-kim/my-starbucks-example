369d5e368a3b1ba72ce932d70f7e6febafb1872c
seongwoo-kim <xtrasy1@gmail.com>
2021-09-25 19:47:40 +0900

    notice promotion 영역 방식 수정 및 js 부분 수정

--- js/main.js
+++ js/main.js
@@ -16,8 +16,7 @@ searchInputEl.addEventListener('blur', function () {
 })
 
 // Badge
-
-const badgeEl = document.querySelector('.badges')
+const badgeEl = document.querySelector('.badges');
 
 window.addEventListener('scroll', _.throttle(function () {
   if (window.scrollY > 500) {
@@ -54,34 +53,31 @@ new Swiper('.notice-left-text .swiper-container', {
 });
 
 new Swiper('.promotion .swiper-container', {
-  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
-  spaceBetween: 10, //슬라이드 사이 여백
-  centeredSlides: true, // 1번 슬라이드가 가운데로 보이기
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
   loop: true,
-  // autoplay: {
-  //   delay: 5000
-  // },
   pagination: {
-    el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
-    clickable: true //사용자의 페이지 번호 요소 제어
+    el: '.promotion .swiper-pagination',
+    clickable: true
   },
   navigation: {
     prevEl: '.promotion .swiper-prev',
     nextEl: '.promotion .swiper-next'
   }
-
 });
 
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
+
 promotionToggleBtn.addEventListener('click', function () {
-  isHidePromotion = !isHidePromotion
   if (isHidePromotion) {
-    // 숨김 처리!
-    promotionEl.classList.add('hide');
-  } else {
     //보임 처리!
     promotionEl.classList.remove('hide');
+  } else {
+    // 숨김 처리!
+    promotionEl.classList.add('hide');
   }
+  isHidePromotion = !isHidePromotion;
 })
